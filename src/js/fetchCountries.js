c27a1a262b0bb55d1bf30538abdaa144ccc5e145
Andrii Ivaniuk <andrey.ivanjuk@gmail.com>
2023-04-01 18:53:41 +0300

    almost done

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,6 @@
 export function fetchCountries(name) {
     const BASE_URL = 'https://restcountries.com/v3.1/name/';
-    fetch(BASE_URL + `${name}?fields=name,capital,population,flags,languages`)
-        .then(r => r.json())
+
+    return fetch(BASE_URL + `${name}?fields=name,capital,population,flags,languages`)
+        .then(r => { return r.json(); }).then(data => {return data })
 }
--- src/js/index.js
+++ src/js/index.js
@@ -15,6 +15,57 @@ refs.inputField.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
     e.preventDefault();
-  
-  const countryName = e.target.value.trim();
+  const countryName = refs.inputField.value.trim();
+
+  if (!countryName) {
+    clearRendered();
+    return;
+  }
+
+  fetchCountries(countryName).then(countriesRender)
+}
+
+function countriesRender(country) {
+  clearRendered();
+
+  if (country.length > 10) {
+    return Notify.info("Too many matches found. Please enter a more specific name.")
+  }
+
+  if (country.length > 2 || country.length <= 10) {
+    const markupList = country.map(({ name, flags }) => {
+      return `<li class="country-list__item">
+        <img class="country-list__image" src="${flags.svg}" width = "80" alt="${flags.alt}">
+        <h2 class="country-list__title">${name.official}</h2>
+        </li>`
+    })
+      .join('');
+    
+    refs.countryList.insertAdjacentHTML('beforeend', markupList)
+  }
+
+  if (country.length === 1) {
+    const markup = country.map(({ name, capital, population, flags, languages }) => {
+      return `<div class="country-info__heading">
+            <img class="country-info__image" src="${flags.svg}" width ="80" alt="${flags.alt}">
+            <h2>${name.official}</h2>
+        </div>
+        <div>
+            <p><b>Capital:</b> ${capital}</p>
+            <p><b>Population:</b> ${population}</p>
+            <p><b>Languages:</b> ${Object.values(languages).join(', ')}</p>
+        </div>`
+    })
+
+    refs.countryInfo.insertAdjacentHTML('beforeend', markup)
+  }
+
+  if (!country.length) {
+    return Notify.failure('Oops, there is no country with that name');
+  };
+}
+
+function clearRendered() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 }
